8e475d01e0fc52d7bb6741a5463137e2f061f764
Levko <lbeniakh@kse.org.ua>
2023-02-15 15:40:51 +0200

    Tokenization made by Levko

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+var input = Console.ReadLine();
+var operators = new char[] { '+', '-', '/', '*', '^', '(', ')' };
+var buffer = "";
+List<string> tokens = new List<string>();
 
-Console.WriteLine("Hello, World!");
+foreach (var character in input)
+{
+    if ((character == '-' && buffer is "" && tokens[^1] == "(") || Char.IsDigit(character))
+    {
+        buffer += character;
+    }
+    else if (operators.Contains(character))
+    {
+        if (buffer != "")
+        {
+            tokens.Add(buffer);
+            buffer = "";
+        }
+        tokens.Add(character.ToString());
+    }
+}
+
+if (buffer != "")
+{
+    tokens.Add(buffer);
+}
+
+foreach (var token in tokens)
+{
+    Console.WriteLine(token);
+}
