6f2127a2f810eae86fcd0766ec09272149001363
Levko <lbeniakh@kse.org.ua>
2023-02-17 11:20:42 +0200

    Added ArrayList

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿
+using System.Collections;
+
 var input = Console.ReadLine();
 var operators = new char[] { '+', '-', '/', '*', '^', '(', ')' };
 var buffer = "";
-List<string> tokens = new List<string>();
+var tokens = new ArrayList();
 
 foreach (var character in input)
 {
-    if ((character == '-' && (tokens.Count == 0 || buffer is "" && tokens[^1] == "(")) || Char.IsDigit(character))
+    if ((character == '-' && (tokens.Count() == 0 || buffer is "" && tokens.GetAt(tokens.Count() - 1) == "(")) || Char.IsDigit(character))
     {
         if (character == '-' && buffer.Length != 0)
         {
@@ -35,7 +37,80 @@ if (buffer != "")
     tokens.Add(buffer);
 }
 
-foreach (var token in tokens)
+foreach (var token in tokens.GetElements())
 {
     Console.WriteLine(token);
+}
+
+public class ArrayList
+{
+    private string[] _array = new string[10];
+    private int _pointer = 0;
+
+    public void Add(string element)
+    {
+        _array[_pointer] = element;
+        _pointer++;
+        if (_pointer == _array.Length)
+        {
+            var extendedArray = new string[_array.Length * 2];
+            for (var i = 0; i < _array.Length; i++)
+            {
+                extendedArray[i] = _array[i];
+            }
+
+            _array = extendedArray;
+        }
+    }
+
+    public void Remove(string element)
+    {
+        for (var i = 0; i < _pointer; i++)
+        {
+            if (_array[i] == element)
+            {
+                for (var j = i; j < _pointer - 1; j++)
+                {
+                    _array[j] = _array[j + 1];
+                }
+
+                _pointer--;
+                return;
+            }
+        }
+    }
+
+    public string GetAt(int index)
+    {
+        return _array[index];
+    }
+
+    public int IndexOf(string value)
+    {
+        for (var i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] == value)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public bool Contains(string element)
+    {
+        return IndexOf(element) != -1;
+    }
+
+    public int Count()
+    {
+        return _pointer;
+    }
+
+    public string[] GetElements()
+    {
+        return _array[.._pointer];
+    }
+
 }
